655d56df02bfd0c13900602e0b3a7c2246015ca5
Paul Kretschel <paul.kretschel@icloud.com>
2019-11-18 15:15:36 +0100

    Prevent "dragging" of images

--- scripts/wrap-images.js
+++ scripts/wrap-images.js
@@ -1,6 +1,7 @@
 hexo.extend.filter.register("after_post_render", data => {
   data.content = data.content.replace(
-    /<p>(<img .+>)<\/p>/g,
-    (_match, imgTag) => `<div class="img">${imgTag}</div>`
+    /<p><img (.+)><\/p>/g,
+    (_match, imgProps) =>
+      `<div class="img"><img ${imgProps} draggable="false"></div>`
   );
 });
